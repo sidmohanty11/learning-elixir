2633ec62b1e6715d66fe1308cd7459bb047d0bbc
sidmohanty11 <sidmohanty11@gmail.com>
2021-10-24 15:56:50 +0530

    more pattern matching

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -47,11 +47,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "That file does not exist"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, reason} -> "That file does not exist, #{reason}"
     end
   end
 end
